f5f2461a43281996853463c158d04796a6b40d46
Gilda Eklof <gilda@eklof.se>
2021-11-16 13:53:35 +0100

    changed back to ring geometry

--- src/Circles/Background/index.js
+++ src/Circles/Background/index.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, createContext } from 'react';
 import { useFrame } from '@react-three/fiber';
+import { BackSide } from 'three';
 import Rings from '../Rings';
 import Particles from '../Particles';
 import Spheres from '../Spheres';
@@ -70,18 +71,18 @@ const Background = ({ num, analyser, player, play, ...rest }) => {
 
   return (
     <mesh ref={mesh} position={[0, 0, 0]} rotation={[0, 0, 0]} {...rest}>
-      {/* <sphereBufferGeometry attach="geometry" args={[100]} />
+      <sphereBufferGeometry attach="geometry" args={[100]} />
       <meshBasicMaterial
         attach="material"
         //Changes the background
         // + 100=hue 100%=background saturation 50%=lightness
         color={`hsl(0, 0%, 10%)`}
         side={BackSide}
-      /> */}
+      />
       <soundContext.Provider value={soundArray}>
-        {/* {makeRings(num)} */}
+        {makeRings(num)}
         {makeParticles(num)}
-        {makeSpheres(num)}
+        {/* {makeSpheres(num)} */}
       </soundContext.Provider>
     </mesh>
   );
--- src/Circles/Rings/index.js
+++ src/Circles/Rings/index.js
@@ -4,56 +4,17 @@ import { DoubleSide } from 'three';
 import { MeshWobbleMaterial, MeshDistortMaterial } from '@react-three/drei';
 
 const Rings = ({ index }) => {
-  // distance between lines
   const height = useContext(soundContext)[index] / 100;
   return (
-    // centers the mesh
     <mesh position={[0, 0, Math.max(0, height)]}>
-      <torusBufferGeometry
+      <ringBufferGeometry
         attach="geometry"
-        args={[
-          index * 1.9,
-          0.5,
-          10,
-          600,
-
-          index * 7,
-          0.6,
-          10,
-          600,
-          ,
-          /* index * 0.3 + 1.2 */ // index * 0.3 + 1.3,
-          // index * 0.3 + 1.2 /* index * 0.3 + 1.2 */,
-          100,
-        ]}
-        // <ringBufferGeometry
-        //   attach="geometry"
-        //   args={[
-        //     index * 0.2 + 0.2,
-        //     index * 0.2 + 0.1 /* index * 0.3 + 1.2 */,
-        //     // index * 0.3 + 1.3,
-        //     // index * 0.3 + 1.2 /* index * 0.3 + 1.2 */,
-        //     1000,
-        //   ]}
+        args={[index * 0.3 + 1.3, index * 0.3 + 1.2, 1000]}
       />
-      {/* <meshBasicMaterial
+      <meshBasicMaterial
         attach="material"
-        // Changes how many colors there are
-        // 100%=color 50%=lightness
-        color={`hsl(${height * 200}, 60%, 60%)`}
+        color={`hsl(${height * 128}, 60%, 60%)`}
         side={DoubleSide}
-      /> */}
-      <meshPhysicalMaterial
-        attach="material"
-        // Changes how many colors there are
-        // 100%=color 50%=lightness
-        color={`hsl(${height * 100}, 50%, 20%)`}
-        side={DoubleSide}
-      />
-      <directionalLight
-        position={[0, 0, 20]}
-        lookAt={[10, 20, 40]}
-        color={0xff0000}
       />
     </mesh>
   );
